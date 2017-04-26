cd614e5e65684ee41c7eec2e92aac12fa783275e
Tony Storti <tonystorti@gmail.com>
2017-04-26 14:21:18 -0500

    simplified code for radio button

--- logic.js
+++ logic.js
@@ -3,17 +3,6 @@
          //assign zipcode from user input and create API call url.
          var unitType = $("input[name=radioTemp]:checked").val();
          var zip = $("#zipCode").val();
-         console.log(unitType);
-         if (unitType == 1){
-            unitType = "imperial";
-         }
-         else if(unitType == 2){
-            unitType ="metric";
-         }
-         else{
-           $(".description").html("Please choose Farenheit or Celcius.") 
-         }
-         console.log(unitType);
          var url ='http://api.openweathermap.org/data/2.5/weather?zip='+zip+',us&units='+unitType+'&appid=a3952d609786e4d345c7162a226c4833';
          console.log(url);
          
