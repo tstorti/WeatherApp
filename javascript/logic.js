4a3e453d2af3e6d9dd3b4ea7a3d3dd49f49273cd
Tony Storti <tonystorti@gmail.com>
2017-05-10 14:26:51 -0500

    updated css with bootstrap

--- javascript/logic.js
+++ javascript/logic.js
@@ -4,15 +4,15 @@
          var unitType = $("input[name=radioTemp]:checked").val();
          var zip = $("#zipCode").val();
          var url ='http://api.openweathermap.org/data/2.5/weather?zip='+zip+',us&units='+unitType+'&appid=a3952d609786e4d345c7162a226c4833';
-         console.log(url);
          
          //API call for current weather
-         $.getJSON(url, function(json){
+            $.getJSON(url, function(json){
             $(".description").html(json.weather[0].description);
             $("#temp").html("Temperature " + json.main.temp);
             $("#name").html("Today's Weather for " + json.name +":");
-            $("#icon").attr("style", "visibility:visible");
             $("#icon").attr("src", "http://openweathermap.org/img/w/"+json.weather[0].icon+".png");
+            $("#output").attr("style", "visibility:visible");
+
          });
       });
    });
